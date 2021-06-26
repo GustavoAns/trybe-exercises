cd1857d5be0c0bc953dba66e261bed57589b4f03
GustavoAns <gugasato9@live.com>
2021-06-25 21:29:01 -0300

    Ex 4 Feito

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let n = 5;
+let n = 9;
 let quadrado = ""
 
 // Ex 1
@@ -27,35 +27,25 @@ let quadrado = ""
 // console.log(quadrado)
 
 // Ex 4
+// "Consultado : https://www.techiedelight.com/replace-character-specified-index-javascript/"
+//
+String.prototype.replaceAt = function(index, replacement) {
+    //Validacao//
+    if (index >= this.length) {
+        return this.valueOf();
+    }
+    //Em (1,A) // ele vai até a casa 1 e subistitui por A e continua até o final //
+ return this.substring(0, index) + replacement + this.substring(index + 1);
+}
+let piramide = "";
+for (var i = 0; i < Math.trunc(n/2); i++) {
+    piramide += ' ';
+    }
+    piramide += '*';
 
-// let piramide = "";
-// for (var i = 0; i < Math.trunc(n/2); i++) {
-//     piramide += ' ';
-//     }
-//     piramide += '*';
-
-//     console.log(piramide)
-// for (var i2 = 1 ; i2 <= Math.trunc(n/2) ; i2++){
-//     console.log((Math.trunc(n/2) - i2))
-//     piramide[(Math.trunc(n/2) - i2)] = "*";
-//     piramide += "*";
-//     console.log(piramide)
-// }
-
-// let lista = new Array(n);
-// for (var i = 0 ; i < n ; i++){
-//     lista[i] = " ";
-// }
-//     lista[Math.trunc(n/2)] = "*";
-//     console.log(lista)
-// for (var i2 = 1 ; i2 < Math.trunc(n/2) ; i2++){
-//     lista[(Math.trunc(n/2) - i2)] = "*";
-//     lista[(Math.trunc(n/2) + i2)] = "*";
-//     console.log(lista)
-// }
-
-
-let lista2 = 'poldo';
-
-    lista2[2] = "*";
-    console.log(lista2)
+    console.log(piramide)
+for (var i2 = 1 ; i2 <= Math.trunc(n/2) ; i2++){
+    piramide = piramide.replaceAt(Math.trunc(n/2) - i2,'*');
+    piramide += "*";
+    console.log(piramide)
+}
