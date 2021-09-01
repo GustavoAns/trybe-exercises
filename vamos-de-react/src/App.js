e9020fbae404ba2463714d269d06a1b3ccc9c444
GustavoAns <gugasato9@live.com>
2021-09-01 14:02:24 -0300

    Save

---
+++ vamos-de-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import pokemonsInfo from './components/pokemonsInfo';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <pokemonsInfo />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ vamos-de-react/src/components/pokemonsInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class pokemonsInfo extends React.Component {
+  render() {
+
+    return (
+      
+      <section>
+      <h1>Teste</h1>
+      </section>
+    )
+  }
+}
+
+export default pokemonsInfo
